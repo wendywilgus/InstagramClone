2d2c207ecff8f2e777a30d80ce6b7ed77716b36e
Wendy Wilgus <wilguswendy@gmail.com>
2023-01-26 09:59:38 -0500

    initial files

---
+++ component/loginPage/LoginPage.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import inst_image from '../../images/9364675fb26a.svg';
+import inst_logo from '../../images/logoinsta.png'
+
+class LogInPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { }
+    }
+    render() {
+        return(
+            <div>
+                <Grid container>
+                    <Grid item xs={3}>
+                        Hello
+                    </Grid>
+                    <Grid item xs={6}>
+                        <div>
+                            <div>
+                                <img 
+                                    src={inst_image}
+                                    alt=""
+                                    width="450"
+                                    />
+                            </div>
+                            <div>
+                                <img 
+                                    src={inst_logo}
+                                    alt=""
+                                    width="450"
+                                    />
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={3}>
+                        Wendy
+                    </Grid>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default LogInPage;
